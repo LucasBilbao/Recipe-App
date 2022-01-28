e76b626cab712bd984c7d88fbbfe288b3586d133
LucasBilbao <lucasbm789@gmail.com>
2022-01-28 23:01:52 +0400

    feat: implement new modules
    add the http client module and other components that were added in the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 import { RecipeSummaryComponent } from './components/recipe-summary/recipe-summary.component';
 import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
+import { EditNewRecipeComponent } from './components/edit-new-recipe/edit-new-recipe.component';
+import { RecipeService } from './services/recipe.service';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
-  declarations: [AppComponent, RecipeListComponent, RecipeSummaryComponent, RecipeDetailsComponent],
+  declarations: [
+    AppComponent,
+    RecipeListComponent,
+    RecipeSummaryComponent,
+    RecipeDetailsComponent,
+    EditNewRecipeComponent,
+    LoaderComponent,
+  ],
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot([
+      {
+        path: 'editnewrecipe',
+        component: EditNewRecipeComponent,
+      },
       {
         path: 'recipes',
         component: RecipeListComponent,
@@ -28,8 +43,9 @@ import { RecipeDetailsComponent } from './components/recipe-details/recipe-detai
         pathMatch: 'full',
       },
     ]),
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [RecipeService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
