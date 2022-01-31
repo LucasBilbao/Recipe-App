9a88e2dce2003f098bf47cf3a96c52026c4b9d74
LucasBilbao <lucasbm789@gmail.com>
2022-01-31 18:17:40 +0400

    feat: expand the add new recipe method
    the add new recipe method is now supporting images

--- src/app/services/recipe.service.ts
+++ src/app/services/recipe.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@angular/core';
 
 import { Recipe, RecipePayload, RecipesPayload } from '../models/recipe.model';
 import { Observable } from 'rxjs/internal/Observable';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 const RECIPE_SERVER = 'http://localhost:4200';
 
@@ -10,7 +17,7 @@ const RECIPE_SERVER = 'http://localhost:4200';
   providedIn: 'root',
 })
 export class RecipeService {
-  httpOptions: Object;
+  httpOptions: object;
 
   constructor(private http: HttpClient) {
     this.httpOptions = {
@@ -21,7 +28,9 @@ export class RecipeService {
   }
 
   getAllRecipes(): Observable<RecipesPayload> {
-    return this.http.get<RecipesPayload>(`${RECIPE_SERVER}/v1/recipes.json`);
+    return this.http
+      .get<RecipesPayload>(`${RECIPE_SERVER}/v1/recipes.json`)
+      .pipe(catchError(this.handleError));
   }
 
   getRecipeById(id: number): Observable<RecipePayload> | undefined {
@@ -30,29 +39,64 @@ export class RecipeService {
       return;
     }
 
-    return this.http.get<RecipePayload>(
-      `${RECIPE_SERVER}/v1/recipes/${id}.json`
-    );
+    return this.http
+      .get<RecipePayload>(`${RECIPE_SERVER}/v1/recipes/${id}.json`)
+      .pipe(catchError(this.handleError));
   }
 
-  addRecipe(recipe: Recipe): Observable<RecipePayload> {
-    return this.http.put<RecipePayload>(
-      `${RECIPE_SERVER}/v1/recipes.json`,
-      recipe,
-      this.httpOptions
-    );
+  addRecipe(recipe: Recipe, files: any): Observable<Recipe> {
+    return new Observable<Recipe>((outer_observable) => {
+      this.http
+        .put<RecipePayload>(
+          `${RECIPE_SERVER}/v1/recipes.json`,
+          recipe,
+          this.httpOptions
+        )
+        .subscribe((recipePayload) => {
+          const finalRecipe: Recipe = recipePayload.data;
+          const formData: FormData = new FormData();
+
+          if (files.coverPhoto) {
+            const file: File = files.coverPhoto;
+            formData.append('coverPhoto', file, file.name);
+          }
+
+          if (files.instructionPhotos) {
+            for (let i = 0; i < files.instructionPhotos.length; i += 1) {
+              if (files.instructionPhotos[i]) {
+                const file: File = files.instructionPhotos[i];
+                formData.append(`instruction_photos_${i}`, file, file.name);
+              }
+            }
+          }
+
+          this.http
+            .post<RecipePayload>(
+              `${RECIPE_SERVER}/v1/recipes/${finalRecipe.id}/images`,
+              formData
+            )
+            .subscribe((sth) => {
+              outer_observable.next(sth.data);
+              outer_observable.complete();
+            });
+        });
+    });
   }
 
-  getNewRecipeID(): number {
-    //   if (this.recipes.length === 0) return 1;
-
-    //   let maxID = this.recipes[0].id;
-
-    //   for (let i = 1; i < this.recipes.length; i += 1) {
-    //     if (this.recipes[i].id > maxID) maxID = this.recipes[i].id;
-    //   }
-
-    //   return maxID + 1;
-    return -1;
+  private handleError(error: HttpErrorResponse): Observable<never> {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred: ', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        JSON.stringify(error, null, 2) +
+          `Backend returned code ${error.status}`,
+        `body was: ${error.error}`
+      );
+    }
+    // return an observable with a user-facing error message
+    return throwError(() => error.error);
   }
 }
